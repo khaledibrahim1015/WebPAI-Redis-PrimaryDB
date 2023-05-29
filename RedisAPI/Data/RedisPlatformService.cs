f13bc34eb6badb3a0b73942f9c9c14aea531fafa
KhaledIbrahim1015 <ibrahimkhaled136@gmail.com>
2023-05-29 05:37:33 +0300

    implement redisPlatFormService with set and string

---
+++ RedisAPI/Data/RedisPlatformService.cs
@@ -0,0 +1,52 @@
+using System.Text.Json;
+using StackExchange.Redis;
+
+namespace RedisAPI;
+
+public class RedisPlatformService : IPlatformService
+{
+    private readonly IDatabase _redis;
+     //private readonly IConnectionMultiplexer _redis;
+
+     public RedisPlatformService(IConnectionMultiplexer redis)
+     {
+
+        _redis = redis.GetDatabase();
+
+     }
+
+    public void CreatePlatform(Platform plat)
+    {
+        if(plat == null)
+            throw new ArgumentOutOfRangeException(nameof(plat));
+
+        _redis.StringSet(plat.Id, JsonSerializer.Serialize(plat));
+        _redis.SetAdd("setPlatform",JsonSerializer.Serialize(plat));
+        
+    }
+
+    public IEnumerable<Platform?>? GetAllPlatform()
+    {
+
+       var completeSetPlatForm  = _redis.SetMembers("setPlatform");
+
+       if(completeSetPlatForm.Length > 0)
+        {
+            var obj =  Array.ConvertAll(completeSetPlatForm, val => JsonSerializer.Deserialize<Platform>(val)).ToList();
+            return obj;
+        }
+        return default;
+        
+    }
+
+    public Platform? GetPlatformById(string id)
+    {
+        
+       var plat= _redis.StringGet(id);
+
+        if(!string.IsNullOrEmpty(plat))
+            return JsonSerializer.Deserialize<Platform>(plat);
+        
+        return default;
+    }
+}
