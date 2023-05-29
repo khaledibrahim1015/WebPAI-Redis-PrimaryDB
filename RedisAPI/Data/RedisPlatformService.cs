0b441fcb2f6d637dfe460c6d54060c5dd2fea92a
KhaledIbrahim1015 <ibrahimkhaled136@gmail.com>
2023-05-29 05:52:55 +0300

    Refactor RedisPlatformService with Hashing

--- RedisAPI/Data/RedisPlatformService.cs
+++ RedisAPI/Data/RedisPlatformService.cs
@@ -17,36 +17,81 @@ public class RedisPlatformService : IPlatformService
 
     public void CreatePlatform(Platform plat)
     {
-        if(plat == null)
+       
+       if(plat ==  null )
             throw new ArgumentOutOfRangeException(nameof(plat));
-
-        _redis.StringSet(plat.Id, JsonSerializer.Serialize(plat));
-        _redis.SetAdd("setPlatform",JsonSerializer.Serialize(plat));
         
+        _redis.HashSet("HashPlatform",new HashEntry[]{
+            new HashEntry(plat.Id,JsonSerializer.Serialize(plat))
+        });
     }
 
     public IEnumerable<Platform?>? GetAllPlatform()
     {
-
-       var completeSetPlatForm  = _redis.SetMembers("setPlatform");
-
-       if(completeSetPlatForm.Length > 0)
-        {
-            var obj =  Array.ConvertAll(completeSetPlatForm, val => JsonSerializer.Deserialize<Platform>(val)).ToList();
-            return obj;
-        }
-        return default;
+        
+      var completehashPlatForm=  _redis.HashGetAll("HashPlatform");
+      if(completehashPlatForm.Length > 0 )
+         return Array.ConvertAll(completehashPlatForm, val => JsonSerializer.Deserialize<Platform>(val.Value)).ToList();
+      
+      return default;
         
     }
 
     public Platform? GetPlatformById(string id)
     {
         
-       var plat= _redis.StringGet(id);
+        var plat = _redis.HashGet("HashPlatform",id);
 
         if(!string.IsNullOrEmpty(plat))
             return JsonSerializer.Deserialize<Platform>(plat);
-        
-        return default;
+        return null;
+
+
+
     }
+
+    // public void CreatePlatform(Platform plat)
+    // {
+    //     if(plat == null)
+    //         throw new ArgumentOutOfRangeException(nameof(plat));
+
+    //     _redis.StringSet(plat.Id, JsonSerializer.Serialize(plat));
+    //     _redis.SetAdd("setPlatform",JsonSerializer.Serialize(plat));
+
+    // }
+
+    // public IEnumerable<Platform?>? GetAllPlatform()
+    // {
+
+    //    var completeSetPlatForm  = _redis.SetMembers("setPlatform");
+
+    //    if(completeSetPlatForm.Length > 0)
+    //     {
+    //         var obj =  Array.ConvertAll(completeSetPlatForm, val => JsonSerializer.Deserialize<Platform>(val)).ToList();
+    //         return obj;
+    //     }
+    //     return default;
+
+    // }
+
+    // public Platform? GetPlatformById(string id)
+    // {
+
+    //    var plat= _redis.StringGet(id);
+
+    //     if(!string.IsNullOrEmpty(plat))
+    //         return JsonSerializer.Deserialize<Platform>(plat);
+
+    //     return default;
+    // }
+
+
+
+
+
+
+
+
+
+
 }
